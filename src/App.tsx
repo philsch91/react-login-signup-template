16ea55bb7d3f7207a70712e52cd06411af0aca1c
philipp <philipp@Philipps-MacBook.local>
2020-04-04 12:34:17 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import { LoginComponent } from './components/login.component';
+import { SignupComponent } from './components/signup.component';
 
 function App() {
+  /*
   return (
+    <h3>React Login and Sign Up Template</h3>
+  );
+  */
+  return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>Login and Signup Template</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={LoginComponent} />
+            <Route path="/sign-in" component={LoginComponent} />
+            <Route path="/sign-up" component={SignupComponent} />
+          </Switch>
+        </div>
+      </div>
     </div>
+    </Router>
   );
 }
 
